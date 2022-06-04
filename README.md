2daaa11069789e74fe7bd84b0b5cbe39dc661e1c
kecven <and.tetka@gmail.com>
2022-06-04 22:01:28 +0300

    add wiki

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # AFK
 
 Universal clicker based on JavaScript
+
+## Run clicker
+    ./afk.sh ./script.afk
+
+## Documentation
+![Documentation](https://github.com/kecven/AFK/tree/master/docs/)
--- afk.sh
+++ afk.sh
@@ -2,4 +2,4 @@
 
 ./gradlew clean shadowJar
 
-java -jar -client -Xms1m -Xmx1024m  -XX:+OptimizeStringConcat -XX:CompileThreshold=500 -XX:MaxHeapFreeRatio=90 -XX:MinHeapFreeRatio=10 ./build/libs/AFK-0.0.4-all.jar script.afk
+java -jar -client -Xms1m -Xmx1024m  -XX:+OptimizeStringConcat -XX:CompileThreshold=500 -XX:MaxHeapFreeRatio=90 -XX:MinHeapFreeRatio=10 ./build/libs/AFK-0.0.4-all.jar $1
