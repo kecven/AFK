e992ad08642d771baebd21f13d43eabfa1c35bbc
kecven <and.tetka@gmail.com>
2022-06-11 14:58:15 +0300

    upgrade gradle and java version

--- src/main/java/net/autoforkey/Main.java
+++ src/main/java/net/autoforkey/Main.java
@@ -1,33 +1,22 @@
 package net.autoforkey;
 
-/*
-набор модулей:
-
- */
-
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 /**
- *
  * Created by andrei on 05.07.15.
  */
 public class Main {
     public static ExecutorService executors = Executors.newCachedThreadPool();
-    public static void main(String[] args) throws InterruptedException {
-
+    public static void main(String[] args) {
         for(int i = 0; i < args.length; i++){
             runScript(new ScriptRunnable("main", args[i]));
         }
-
     }
 
     public static void runScript(ScriptRunnable thread){
         //executors.submit(thread);
         //executors.execute(thread);
         new Thread(thread).start();
-
     }
-
-
 }
